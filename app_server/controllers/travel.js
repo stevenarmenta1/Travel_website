5f1621180bf102ab1cae65cb45fee6eefa694b09
Your Name <you@example.com>
2025-06-04 14:07:32 -0700

    Update module5 branch with new local changes

---
+++ app_server/controllers/travel.js
@@ -0,0 +1,32 @@
+const tripsEndpoint = 'http://localhost:3000/api/trips';
+const options = {
+method: 'GET',
+headers: {
+'Accept': 'application/json'
+}
+}
+//var fs = require('fs');
+//var trips =
+//JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+/* GET travel view */
+const travel = async function(req , res, next) {
+    await fetch(tripsEndpoint, options)
+        .then(res => res.json())
+        .then(json => {
+            let message = null;
+            if(!(json instanceof Array)) {
+                message = json;
+                json = [];
+            }else{
+                if(!json.length) {
+                    message = "No trips exist in our database!";
+                }
+            }
+            res.render('travel', {title: 'Travlr Getaways', trips: json, message});
+        })
+
+    .catch(err => res.status(500).send(e.message));
+};
+
+module.exports = {travel};
